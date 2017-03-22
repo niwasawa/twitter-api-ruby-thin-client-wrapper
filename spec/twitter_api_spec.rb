65ecca71d15621d26c30432a89b53756d0e286f8
Naoki Iwasawa <niwasawa@maigo.info>
2017-03-22 19:41:49 +0900

    add test cases

--- spec/twitter_api_spec.rb
+++ spec/twitter_api_spec.rb
@@ -26,6 +26,45 @@ describe TwitterAPI::Client do
   }
   t = TwitterAPI::Client.new(credential)
 
+  it 'has a method get' do
+    resource_url = 'https://api.twitter.com/1.1/statuses/user_timeline.json'
+    params = {
+      'screen_name' => 'YOUR_SCREEN_NAME',
+      'count' => '1'
+    }
+    expect(t.get(resource_url, params)).not_to be nil
+  end
+
+  it 'has a method post' do
+    resource_url = 'https://api.twitter.com/1.1/statuses/update.json'
+    params = {
+      'status' => 'The text of your status update, typically up to 140 characters.'
+    }
+    expect(t.post(resource_url, params)).not_to be nil
+  end
+
+  it 'has a method search_tweets' do
+    params = {
+      'q' => 'search query',
+      'count' => '1'
+    }
+    expect(t.search_tweets(params)).not_to be nil
+  end
+
+  it 'has a method statuses_mentions_timeline' do
+    params = {
+      'count' => '1'
+    }
+    expect(t.statuses_mentions_timeline(params)).not_to be nil
+  end
+
+  it 'has a method statuses_update' do
+    params = {
+      'status' => 'The text of your status update, typically up to 140 characters.'
+    }
+    expect(t.statuses_update(params)).not_to be nil
+  end
+
   it 'has a method statuses_user_timeline' do
     params = {
       'screen_name' => 'YOUR_SCREEN_NAME',
